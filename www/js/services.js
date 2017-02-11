b2ac72d890287a0abe1d3a19b6fbc799403e1c63
jhl <jhl@aAA.wv.cc.cmu.edu>
2017-02-11 01:29:12 -0500

    1. Leader Board (service.js's data in progress) 2. Profile 3. tabs.html (added new tabs) 4. imgs added and modified photo names 5. service.js (tried to add a new factory for leader board)

--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,47 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+
+
+.factory('Leaders', function() {
+  var leaders = [{
+    id : 0,
+    name: 'Max Ma',
+    current : 10,
+    Longest : 233,
+    face: 'img/max.png'
+  }, {
+    id : 1,
+    name: 'Mike Chen',
+    current : 1,
+    longest : 29,
+    face: 'img/mike.png'
+  }, {
+    id : 2,
+    name: 'Toffer Huang',
+    current : 0,
+    longest : 199,
+    face: 'img/toffer.png'
+  }, {
+    id : 3,
+    name: 'Yue Yin',
+    current : 8,
+    longest : 10,
+    face: 'img/yue.png'
+  }];
+
+  return {
+    all: function() {
+      return leaders;
+    },
+    get: function(leaderID) {
+      for (var i = 0; i < leaders.length; i++) {
+        if (chats[i].id === parseInt(leaderID)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
 });
